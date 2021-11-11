44b1f49d00f17ca022f46bfd294fa2cb3393f960
Rakhi Pundhir <rakhipundhir2711@gmail.com>
2021-11-11 19:58:06 +0530

    day16 of 100daysofcode

---
+++ day16.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+ struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;  
+            if(fast==slow)
+                return true;
+        }
+        return false;
+    }
+};
