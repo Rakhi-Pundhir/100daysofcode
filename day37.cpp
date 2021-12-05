17801a9b6d479f42f00f9ccd4b91eab3e51eaf6d
Rakhi Pundhir <rakhipundhir2711@gmail.com>
2021-12-05 22:28:27 +0530

    day37 of 100daysofcode

---
+++ day37.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+ using namespace std;
+  struct TreeNode {
+      int val;
+     TreeNode *left;
+     TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL),right(NULL) {}
+ };
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL)
+            return NULL;
+        if(root==p || root==q)
+            return root;
+        root->left=lowestCommonAncestor(root->left,p,q);
+        root->right=lowestCommonAncestor(root->right,p,q);
+        if(root->left!=NULL && root->right!=NULL)
+            return root;
+        else
+            return (root->left)?root->left:root->right;
+    }
+};
