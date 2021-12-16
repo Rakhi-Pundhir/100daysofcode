445db281bee22a00a4e7f65ad439e2612d240263
Rakhi Pundhir <rakhipundhir2711@gmail.com>
2021-12-16 22:28:29 +0530

    day47 of 100daysofocde

---
+++ day47.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution 
+{
+    public:
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+        stack<int> st;
+        for(int i=0;i<M.size();i++)
+        {
+            st.push(i);
+        }
+        while(st.size()>=2)
+        {
+            int i=st.top();
+            st.pop();
+            int j=st.top();
+            st.pop();
+            if(M[i][j]==0)
+            st.push(i);
+            else if(M[i][j]==1)
+            st.push(j);
+        }
+        int x=st.top();
+        st.pop();
+        for(int i=0;i<M.size();i++)
+        {
+            if(i!=x)
+            {
+               if(M[i][x]==0 || M[x][i]==1)
+               return -1;
+            }
+        }
+        return x;
+    }
+};
