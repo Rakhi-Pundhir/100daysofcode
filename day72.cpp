f32c30b0feed7327012366a2f5c9cc901ba7ed76
Rakhi Pundhir <rakhipundhir2711@gmail.com>
2022-01-23 23:01:41 +0530

    day72 of 100daysofcode

---
+++ day72.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+const int INF=1e9;
+using namespace std;
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+    int u,v,w;
+    vector<vector<int>> graph;
+    for(int i=0;i<e;i++)
+    {
+        
+        cin>>u>>v>>w;
+        graph.push_back({u,v,w});
+    }
+    int src;
+    cin>>src;
+    vector<int> dist(n,INF);
+    dist[src]=0;
+    for(int i=0;i<n-1;i++)
+    {
+        for(auto it:graph)
+        {
+            u=it[0];
+            v=it[1];
+            w=it[2];
+            dist[v]=min(dist[v],dist[u]+w);
+        }
+    }
+    for(auto d:dist)
+    {
+        cout<<d<<" ";
+    }
+}
